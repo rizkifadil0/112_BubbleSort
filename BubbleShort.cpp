446c69b879e901e6c4f4d7d898c1b2e1c390230d
rixkifadil@gmail.com <rixkifadil@gmail.com>
2025-03-13 16:25:08 +0700

    add procedur inpu and prosedur bubblesort

---
+++ BubbleShort.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int arr[15];
+int n; //input jumlah data
+
+void inputdata (){
+    while(true)
+    {
+        cout << "masukkan jumlah data : ";
+        cin >> n;
+
+        if(n < 15){
+            break;
+        }
+        else{
+            cout << "jumlah data maksimal 15" << endl;
+        }
+    }
+    cout << "===========================" << endl;
+    cout << "masukkan data" << endl;
+    cout << "===========================" << endl;
+    cout << endl;
+
+    for (int i = 0; i < n; i++){
+        cout << "masukkan data ke-" << i +1 << "=";
+        cin >> arr[i];
+    }
+}
+
+void bubblesort(){
+    int pass = 1; //step 1
+    do
+    {
+        
+        for (int j = 0; j <= n-1-pass; j++) // step 2
+        {
+            if (arr[j] > arr[j+1]){ //step 3
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    
+        pass = pass + 1; // step 4
+    } while (pass <= n - 1); //step 5
+    
+
+}
+
+int main (){
+    inputdata();
+}
